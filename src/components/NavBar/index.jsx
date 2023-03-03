a7345257258e417c63ea492124f75b6bd32fab08
Marcelo Folloni <marfolloni@gmail.com>
2023-03-02 23:02:11 -0300

    PreEntrega1+MarceloFolloni

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,23 @@
+import './NavBar.css';
+import CardWidget from '../CardWidget';
+
+function NavBar() {
+    return (
+    <div className ="navbar">
+        <ul>
+            <li>
+                <button>Pantalon</button>
+            </li>
+            <li>
+                <button>Camisa</button>
+            </li>
+            <li>
+                <button>Remera</button>    
+            </li>
+        </ul>
+        < CardWidget />
+    </div>
+    );
+}
+
+export default NavBar;
